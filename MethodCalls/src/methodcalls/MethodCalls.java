8598ae66d1a079bb47cf30fade8034c598a40196
StillWaters <ndivhalenioscar624@gmail.com>
2024-12-20 14:20:32 +0200

    Add files via upload

---
+++ MethodCalls/src/methodcalls/MethodCalls.java
@@ -0,0 +1,18 @@
+package methodcalls;
+
+public class MethodCalls {
+
+    public static void main(String[] args) {
+
+    Biography();    
+    System.out.println("This is my first program");
+    }
+        public static void Biography(){
+    
+        System.out.println("Oscar Siphei");
+        System.out.println("Madombidzha zone 2");
+        System.out.println("Makhado");
+        System.out.println("0920");
+
+    }
+}
